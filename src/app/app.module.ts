f3d62a9c64f9889f3af90b0984b7d0e4dbb89e41
Bellatrix988 <marina.yurova1996@yandex.ru>
2017-10-30 14:25:44 +0300

    Add component tabs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,17 +5,19 @@ import { HttpModule } from '@angular/http';
 import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
-import { ListUpdatesComponent } from './pages/list-updates/list-updates.component';
 import { UpdatesDataService } from './services/updates-data.service';
 import { UsersDataService } from './services/users-data.service';
 import { AppRoutingModule } from './app-routing/app-routing.module';
 import { FormCreateComponent } from './components/form-create/form-create.component';
+import { ListUpdatesComponent } from './components/list-updates/list-updates.component';
+import { TabsNavigateComponent } from './pages/tabs-navigate/tabs-navigate.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ListUpdatesComponent,
-    FormCreateComponent
+    FormCreateComponent,
+    TabsNavigateComponent
   ],
   imports: [
     RouterModule,
---
+++ src/app/pages/tabs-navigate/tabs-navigate.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tabs-navigate',
+  templateUrl: './tabs-navigate.component.html',
+  styleUrls: ['./tabs-navigate.component.css']
+})
+export class TabsNavigateComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
