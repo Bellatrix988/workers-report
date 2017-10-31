aa1be144d648f4f7d35a7442990f22ccb6bcc183
Bellatrix988 <marina.yurova1996@yandex.ru>
2017-10-31 10:17:41 +0300

    Add logic to Tabs Component. Add class Tab

---
+++ src/app/models/tab.ts
@@ -0,0 +1,6 @@
+export class Tab {
+  id: number;
+  title: string;
+  active: boolean;
+  path?: string;
+}
--- src/app/pages/tabs-navigate/tabs-navigate.component.ts
+++ src/app/pages/tabs-navigate/tabs-navigate.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Tab } from '../../models/tab';
 
 @Component({
   selector: 'app-tabs-navigate',
@@ -7,9 +9,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TabsNavigateComponent implements OnInit {
 
-  constructor() { }
+  tabs: Tab[];
+  constructor(private router: Router) { }
 
-  ngOnInit() {
+  onActive(tab?: Tab) {
+    this.tabs.forEach(item => item.active = false);
+    tab.active = true;
   }
 
+  ngOnInit() {
+    console.log(this.router.url);
+    this.tabs = [{ id: 1, title: 'Add', path: '/create', active: false },
+                 { id: 2, title: 'All', path: '/index', active: true },
+                 { id: 3, title: 'My', path: '/index', active: false },
+                 { id: 4, title: 'Bla', path: '/index', active: false }];
+  }
 }
