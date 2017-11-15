4ad1ad4c18152bf9e80a23f5df0546acc5cb5a14
Bellatrix988 <marina.yurova1996@yandex.ru>
2017-11-15 11:54:07 +0300

    Add style for TimeLine: odd/even position

--- src/app/components/time-line/time-line.component.ts
+++ src/app/components/time-line/time-line.component.ts
@@ -6,23 +6,25 @@ import { TimeObj } from '../../models/time-line-data.model';
   templateUrl: './time-line.component.html',
   styleUrls: ['./time-line.component.css']
 })
+
 export class TimeLineComponent implements OnInit {
 
   public timeLineData: TimeObj[];
-
+//export const ONE_DAY = 86400000;
+  // public cur
   constructor() { }
 
   public clickOnDate(item: TimeObj): void {
-    console.log(item.id);
+    console.log(item.date);
   }
 
   ngOnInit() {
     this.timeLineData = [
-      { id: 1, date: new Date() },
-      { id: 2, date: new Date() },
-      { id: 3, date: new Date() },
-      { id: 4, date: new Date() },
-      { id: 5, date: new Date() }
+      { id: 1, date: new Date(2017, 10, 9) },
+      { id: 2, date: new Date(2017, 10, 10) },
+      { id: 3, date: new Date(2017, 10, 13) },
+      { id: 4, date: new Date(2017, 10, 14) },
+      { id: 5, date: new Date(2017, 10, 15) }
     ];
   }
 
